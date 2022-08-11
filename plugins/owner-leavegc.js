69fc81accff452abb326df41ad069eea028ecb46
Misael <101156132+Misaelsharkbot@users.noreply.github.com>
2022-08-11 16:07:37 -0500

    Update owner-leavegc.js

--- plugins/owner-leavegc.js
+++ plugins/owner-leavegc.js
@@ -1,11 +1,11 @@
 let handler = async (m, { conn, args, command }) => {
 	let group = m.chat
-        await m.reply('Sayonara , , ! (≧ω≦)ゞ', m.chat) 
+        await m.reply('El bot saldra del grupo, , ! (≧ω≦)ゞ', m.chat) 
         await  conn.groupLeave(group)
         }
 handler.help = ['leavegc', 'out']
 handler.tags = ['owner']
-handler.command = /^(out|leavegc)$/i
+handler.command = /^(salir|leavegc)$/i
 
 handler.rowner = true
 
